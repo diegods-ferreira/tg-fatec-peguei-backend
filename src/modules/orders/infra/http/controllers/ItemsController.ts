7f6588dd1654bf252db49afcb5aba3567f7f50b2
Danilo Ferreira <danilo1705@gmail.com>
2020-11-27 15:57:34 -0300

    ItemsController updated with show method

--- src/modules/orders/infra/http/controllers/ItemsController.ts
+++ src/modules/orders/infra/http/controllers/ItemsController.ts
@@ -1,9 +1,20 @@
+import ShowItemDetailsService from '@modules/orders/services/ShowItemDetailsService';
 import UpdateItemService from '@modules/orders/services/UpdateItemService';
 import { classToClass } from 'class-transformer';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 export default class ItemsController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { item_id } = request.params;
+
+    const showItemDetails = new ShowItemDetailsService();
+
+    const item = await showItemDetails.execute(item_id);
+
+    return response.json(classToClass(item));
+  }
+
   public async update(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const {
